d0ef1bad3a42b595c7eefd1c48a46dc617693661
Adam Weldon <adam.weldon@zogdigital.com>
2014-10-20 11:00:45 -0700

    this should work

--- pdo/class/classUser.php
+++ pdo/class/classUser.php
@@ -7,13 +7,9 @@
 	{
 		public function insertUser()
 		{
-			// echo 'this is where you insert a user';
-			// printer($this);
 			$query = "INSERT INTO users SET `firstname` = :firstname, `lastname` = :lastname, `email` = :email, `telephone` = :phone, `password` = :password, `created_at` = now()";
 			$prepare = array(':firstname' => $this->first_name, ':lastname' => $this->last_name, ':email' => $this->email, ':phone' => $this->telephone, ':password' => $this->password);		
 			$results=RECORD::execute($query, $prepare);
-			// echo "all done";
-			// exit;
 		}
 
 	}
